b1415b301955e22dd7c60b86285630bc8ae7acaa
justinbusker <jtbusker@icloud.com>
2024-08-29 21:11:33 +0000

    lab1 completed

--- lab1/buggy.cpp
+++ lab1/buggy.cpp
@@ -12,31 +12,39 @@ class Shape {
     int vertices;
     Point** points;
 
+public:
+
     Shape (int _vertices) {
         vertices = _vertices;
         points = new Point*[vertices+1];
     }
 
     ~Shape () {
+		for(int i = 0; i <= vertices; ++i){
+			delete points[i];
+		}
+		delete[] points;
+		
     }
 
-    void addPoints (Point pts[]) {
+    void addPoints (Point* pts) {
         for (int i = 0; i <= vertices; i++) {
+			points[i] = new Point();
             memcpy(points[i], &pts[i%vertices], sizeof(Point));
         }
     }
 
-    double* area () {
+    double area () {
         int temp = 0;
-        for (int i = 0; i <= vertices; i++) {
+        for (int i = 0; i < vertices; i++) {
             // FIXME: there are two methods to access members of pointers
             //        use one to fix lhs and the other to fix rhs
             int lhs = points[i]->x * points[i+1]->y;
-            int rhs = points[i+1]->x * points[i]->y;
+            int rhs =(*points[i+1]).x * (*points[i]).y;
             temp += (lhs - rhs);
         }
         double area = abs(temp)/2.0;
-        return &area;
+        return area;
     }
 };
 
@@ -47,15 +55,17 @@ int main () {
     //          tri2 = (1, 2)
     //          tri3 = (2, 0)
 
+	// First method of creating Point tri1
 	Point tri1;
 	tri1.x = 0;
 	tri1.y = 0;
 
+	// Second Method creating tri2
 	Point tri2(1,2);
 
+	// Third method creating tri3
 	Point tri3 = {2,0};
 
-	
     // adding points to tri
     Point triPts[3] = {tri1, tri2, tri3};
     Shape* tri = new Shape(3);
@@ -68,10 +78,11 @@ int main () {
     //          quad3 = (2, 2)
     //          quad4 = (2, 0)
 
-	Point quad1 = {0,0};
-	Point quad2 = {0,2};
-	Point quad3 = {2,2};
-	Point quad4 = {2,0};
+	// Using method 2 (in tri above) to create quad structs
+	Point quad1(0,0);
+	Point quad2(0,2);
+	Point quad3(2,2);
+	Point quad4(2,0);
 
     // adding points to quad
     Point quadPts[4] = {quad1, quad2, quad3, quad4};
@@ -79,4 +90,10 @@ int main () {
     quad->addPoints(quadPts);
 
     // FIXME: print out area of tri and area of quad
+		
+	std::cout << "Triangle Area: " << tri->area() << std::endl;
+	std::cout << "Quad Area: " << quad->area() << std::endl;
+
+	delete tri;
+	delete quad;
 }
